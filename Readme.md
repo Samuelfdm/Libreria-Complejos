f763057071b62aec954ae2e1b6e2f234f7bd0bb5
Samuelfdm <samuelfdm14@gmail.com>
2022-09-10 00:13:35 -0500

    commit

--- Readme.md
+++ Readme.md
@@ -1,20 +1,48 @@
 # Libreria de números complejos
-#### Operaciones con números complejos
-#### En esta librería se encuentra una serie de operaciones para numeros complejos, entre ellas encontramos:
-#### - Multiplicación
-#### - Resta
-#### - Suma
-#### - División
-#### - Módulo
-#### - Conjugado 
-#### - Conversión entre representaciones polar y cartesiano
-#### - Fase
 
+## Versión #1: Operaciones Basicas con numeros complejos.
+
+#### Operaciones con números complejos.
+#### En esta primera versión de la librería encontramos una serie de operaciones basicas entre numeros complejos, tales como:
+#### 1.- Multiplicación
+#### 2.- Resta
+#### 3.- Suma
+#### 4.- División
+#### 5.- Módulo
+#### 6.- Conjugado 
+#### 7.- Conversión entre representaciones polar y cartesiano
+#### 8.- Fase
+
+## Versión #2: Operaciones para vectores y Matrices.
+
+#### En esta segunda versión de la librería encontramos operaciones más avanzadas que incluyen vectores y matrices, tales como:
+
+#### 1.- Adición de vectores complejos.
+#### 2.- Inverso (aditivo) de un vector complejo.
+#### 3.- Multiplicación de un escalar por un vector complejo.
+#### 4.- Adición de matrices complejas.
+#### 5.- Inversa (aditiva) de una matriz compleja.
+#### 6.- Multiplicación de un escalar por una matriz compleja.
+#### 7.- Transpuesta de una matriz/vector
+#### 8.- Conjugada de una matriz/vector
+#### 9.- Adjunta (daga) de una matriz/vector
+#### 10.- Producto de dos matrices (de tamaños compatibles)
+#### 11.- Función para calcular la "acción" de una matriz sobre un vector.
+#### 12.- Producto interno de dos vectores
+#### 13.- Norma de un vector
+#### 14.- Distancia entre dos vectores
+#### 15.- Revisar si una matriz es unitaria
+#### 16.- Revisar si una matriz es Hermitiana
+#### 17.- Producto tensor de dos matrices/vectores
+
+## Desarrollado con:
+- Python - Lenguaje de Programación
 ## Construido con:
 - [PyCharm](https://www.jetbrains.com/pycharm/) - IDE de Python
 ## Autor:
 - **Samuel Felipe Díaz** - [*Samuelfdm*](https://github.com/Samuelfdm)
 ## Agradecimientos:
+- #### Profesor Luis Benavidez
 - <https://gist.github.com/PurpleBooth/109311bb0361f32d87a2>
 - <https://www.youtube.com/watch?v=SWYqp7iY_Tc>
 - <https://www.markdownguide.org/>
--- libreria.py
+++ libreria.py
@@ -1,19 +1,19 @@
 import math
-
-# Multiplica dos números complejos:
-def multiComplex(a,b):
+#------------PRIMERA VERSIÓN------------
+# Realiza el producto entre dos números complejos:
+def productComplex(a, b):
     return ((a[0]*b[0] - a[1]*b[1]), (a[0]*b[1] + a[1]*b[0]))
 
 # Resta dos números complejos:
-def restaComplex(a,b):
-    return ((a[0]-b[0]),(a[1]-b[1]))
+def restaComplex(a, b):
+    return ((a[0]-b[0]), (a[1]-b[1]))
 
 # Suma dos números complejos:
-def sumComplex(a,b):
-    return ((a[0]+b[0]),(a[1]+b[1]))
+def sumComplex(a, b):
+    return ((a[0]+b[0]), (a[1]+b[1]))
 
 # Divide dos números complejos:
-def diviComplex(a,b):
+def diviComplex(a, b):
     denominador = b[0]**2 + b[1]**2
     numerador1 = a[0] * b[0] + a[1] * b[1]
     numerador2 = b[0] * a[1] - a[0] * b[1]
@@ -44,4 +44,115 @@ def polar_to_cartesianComplex(num):
 def faseComplex(num):
     return math.atan(num[1]/num[0])
 
+#------------SEGUNDA VERSIÓN------------
 # Suma dos vectores complejos:
+def sumVectorComplex(v1, v2):
+    v3 = []
+    for i in range(len(v1)):
+        v3.append(sumComplex(v1[i], v2[i]))
+    return v3
+
+# Devuelve el inverso aditivo de un vector complejo:
+def inversoVectorComplex(v):
+    v2 = []
+    for i in range(len(v)):
+        v2.append(((v[i][0])*(-1), (v[i][1])*(-1)))
+    return v2
+
+# Multiplica un vector complejo por un escalar:
+def escalarVectorComplex(c, v):
+    v2 = []
+    for i in range(len(v)):
+        v2.append(multiComplex(c, v[i]))
+    return v2
+
+# Suma dos matrices complejas:
+def sumMatrizComplex(m1, m2):
+    m3 = []
+    for i in range(len(m1)):
+        m3.append(sumVectorComplex(m1[i], m2[i]))
+    return m3
+
+# Devuelve la inversa aditiva de una matriz compleja:
+def inversaMatrizComplex(m):
+    m2 = []
+    for i in range(len(m)):
+        m2.append(inversoVectorComplex(m[i]))
+    return m2
+
+# Multiplica una matriz compleja por un escalar:
+def escalarMatrizComplex(c, m):
+    m2 = []
+    for i in range(len(m)):
+        m2.append(escalarVectorComplex(c,m[i]))
+    return m2
+
+# Devuelve la traspuesta de una matriz:
+def traspuestaMatrizComplex(m):
+    for i in range(len(m)):
+        for k in range(i+1, len(m[i])):
+            m[i][k], m[k][i] = m[k][i], m[i][k]
+    return m
+
+# Devuelve la traspuesta de un vector:
+def traspuestaVectorComplex(v):
+    return [[v[i]] for i in range(len(v))]
+
+# Devuelve la conjugada de un vector:
+def conjugadaVectorComplex(v):
+    return [(v[i][0], (v[i][1])*-1) for i in range(len(v))]
+
+# Devuelve la conjugada de una matriz:
+def conjugadaMatrizComplex(m):
+    return [conjugadaVectorComplex(m[i]) for i in range(len(m))]
+
+# Devuelve la adjunta de un vector:
+def adjuntaVectorComplex(v):
+    return traspuestaVectorComplex(conjugadaVectorComplex(v))
+
+# Devuelve la adjunta de una matriz:
+def adjuntaMatrizComplex(m):
+    return traspuestaMatrizComplex(conjugadaMatrizComplex(m))
+
+# Realiza el producto entre dos matrices (de tamaños compatibles):
+def productMatrices(m1, m2):
+    a, b, c, d = len(m1), len(m1[0]), len(m2), len(m2[0])
+    if b!=c:
+        return "ES IMPOSIBLE REALIZAR LA OPERACIÓN, REVISE LA ENTRADA"
+    else:
+        m3 = []
+        for i in range(a):
+            filas_m3 = []
+            for k in range(d):
+                acum = (0, 0)
+                for j in range(b):
+                    c1 = m1[i][j]
+                    c2 = m2[j][k]
+                    acum = sumComplex(acum, productComplex(c1, c2))
+                filas_m3.append(acum)
+            m3.append(filas_m3)
+    return m3
+
+# Calcula la acción de una matriz sobre un vector:
+def accionMatrizVectorComplex(m, v):
+    a, b, c = len(m), len(v), len(m[0])
+    if b != c:
+        return "ES IMPOSIBLE REALIZAR LA OPERACIÓN, REVISE LA ENTRADA"
+    else:
+        v_new = []
+        for i in range(a):
+            acum = (0, 0)
+            for k in range(b):
+                c1 = m[i][k]
+                c2 = v[k]
+                acum = sumComplex(acum, productComplex(c1, c2))
+            v_new.append(acum)
+    return v_new
+
+# Calcula el producto interno de dos vectores:
+#def innerProductVectorComplex(v1,v2):
+
+#v1 = [(5, 4), (0, 1)]
+#v2 = [(-13, -2), (5, -7)]
+
+print(innerProductVectorComplex(v1, v2))
